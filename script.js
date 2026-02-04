a323b19d4b91e0abb17a58e61cbc95c180b6e8ed
cunil17 <sajojo237@gmail.com>
2026-02-05 00:14:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+﻿let bpm = 120;
+let playing = false;
+let timer;
+
+const bpmText = document.getElementById("bpm");
+const beat = document.getElementById("beat");
+const playBtn = document.getElementById("play");
+
+function updateBpm() {
+  bpmText.textContent = bpm;
+}
+
+function start() {
+  const interval = 60000 / bpm;
+
+  timer = setInterval(() => {
+    beat.classList.add("active");
+    setTimeout(() => beat.classList.remove("active"), 100);
+  }, interval);
+}
+
+playBtn.onclick = () => {
+  if (!playing) {
+    start();
+    playBtn.textContent = "STOP";
+  } else {
+    clearInterval(timer);
+    playBtn.textContent = "PLAY";
+  }
+  playing = !playing;
+};
+
+document.getElementById("plus").onclick = () => {
+  bpm += 5;
+  updateBpm();
+  if (playing) {
+    clearInterval(timer);
+    start();
+  }
+};
+
+document.getElementById("minus").onclick = () => {
+  bpm = Math.max(40, bpm - 5);
+  updateBpm();
+  if (playing) {
+    clearInterval(timer);
+    start();
+  }
+}
